6aa0c2bc0f3605e84fd2fa23e4e78fb61058f0e6
John Stilwell <johnrstilwell@codeimmersives.com>
2019-07-11 11:40:51 -0400

    changing verbage between answer choices

--- main.js
+++ main.js
@@ -10,5 +10,48 @@ function handleDecision(event) {
   if (userInput === 'east') {
     window.location = './east.html';
   } 
+  
+  if (userInput === 'north') {
+  window.location = './north.html';
+  }
+
+  if (userInput === 'south') {
+    window.location = './south.html';
+  }
+
+  if (userInput === 'west') {
+    window.location = './west.html';
+  }
+
+  if (userInput === 'chicken') {
+    window.location = './chicken.html';
+  }
+
+  if (userInput === 'pizza') {
+    window.location = './pizza.html';
+  }
+
+  if (userInput === 'tacos') {
+    window.location = './tacos.html';
+  }
+
+  if (userInput === 'lamb') {
+    window.location = './lamb.html';
+  }
+
+  if (userInput === 'jameson') {
+    window.location = './jameson.html';
+  }
+
+  if (userInput === 'titos') {
+    window.location = './titos.html';
+  }
+
+  if (userInput === 'ipa') {
+    window.location = './ipa.html';
+  }
+
+  if (userInput === 'corona') {
+    window.location = './corona.html';
   }
 }
